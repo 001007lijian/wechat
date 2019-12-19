0981a2ddac054f6fb2c1a00770721c72d7bba812
lijian <lijian@localhost.localdomain>
2019-12-19 01:59:56 +0000

    网页授权 投票

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -8,7 +8,7 @@ class VoteController extends Controller
 {
     public function index()
     {
-        echo "<pre>"; print_r($_GET);  echo "</pre>";  die;
+//        echo "<pre>"; print_r($_GET);  echo "</pre>";  die;
         $code=$_GET['code'];
         //获取access_token
         $data=$this->getAccessToken($code);
@@ -16,6 +16,23 @@ class VoteController extends Controller
         $userinfo=$this->getUserInfo($data['access_token'],$data['openid']);
 
         //处理业务逻辑
+        $openid=$userinfo['openid'];
+        $key = 'ss:vote:lijian';
+
+        //判断是否已经投过票
+        if (Redis::zrank($key,$userinfo['openid'])){
+            echo "已经投过票了";
+        }else{
+            Redis::Zadd($key,time(),$openid);
+        }
+
+        $total=Redis::zCard($key);  //获取总数
+        echo '投票总人数：'.$total;   echo '</br>';
+        $members=Redis::zRange($key,0,-1,true); //获取所有投票人的openid
+        echo "<pre>";   print_r($members);  echo "</pre>";
+        foreach ($members as $k=>$v){
+            echo "用户： ".$k . ' 投票时间: '. date('Y-m-d H:i:s',$v);echo '</br>';
+        }
     }
 
 
@@ -27,7 +44,22 @@ class VoteController extends Controller
     {
         $url='https://api.weixin.qq.com/sns/oauth2/access_token?appid='.env('APPID').'&secret='.env('SECRET').'&code='.$code.'&grant_type=authorization_code';
         $json_data=file_get_contents($url);
-        $data=json_decode($json_data,true);
-        echo "<pre>";   print_r($data);   echo "</pre>";
+        return json_decode($json_data,true);
+    }
+
+
+    /**
+     * 获取用户基本信息
+     */
+    protected function getUserInfo($access_token,$openid)
+    {
+        $url = 'https://api.weixin.qq.com/sns/userinfo?access_token='.$access_token.'&openid='.$openid.'&lang=zh_CN';
+        $json_data = file_get_contents($url);
+        $data = json_decode($json_data,true);
+        if(isset($data['errcode'])){
+            // TODO  错误处理
+            die("出错了 40001");       // 40001 标识获取用户信息失败
+        }
+        return $data;           // 返回用户信息
     }
 }
